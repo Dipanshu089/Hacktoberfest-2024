c8a12e3631f656d9933f6d002ac249adaa0efda2
Sourav Singh <113691137+onizukahere@users.noreply.github.com>
2022-10-12 14:58:45 +0530

    Create Reverse_Bits_Number.py

---
+++ Reverse_Bits_Number.py
@@ -0,0 +1,29 @@
+
+def reverseBits(n) :
+
+    rev = 0
+
+    # traversing bits of 'n' from the right
+
+    while (n > 0) :
+
+
+        # bitwise left shift 'rev' by 1
+
+        rev = rev << 1
+
+        # if current bit is '1'
+
+        if (n & 1 == 1) :
+
+            rev = rev ^ 1
+
+        # bitwise right shift 'n' by 1
+
+        n = n >> 1
+
+    return rev
+
+n = 11
+
+print(reverseBits(n))
